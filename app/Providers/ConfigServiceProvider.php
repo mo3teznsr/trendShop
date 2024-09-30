414cf2101605c1923f2943c18890c3b721422258
mo3teznsr <mo3teznsr@gmail.com>
2024-09-30 13:53:23 +0200

    Remove cached files and re-add respecting .gitignore

---
+++ app/Providers/ConfigServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\BusinessSetting;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class ConfigServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        \URL::forceScheme('https');
+        try {
+            $timezone = BusinessSetting::where(['type' => 'timezone'])->first();
+            if ($timezone) {
+                Config::set('timezone', $timezone->value);
+                date_default_timezone_set($timezone->value);
+            }
+        } catch (\Exception $ex) {}
+    }
+}
